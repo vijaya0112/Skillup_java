520903a477a3c2c40679aa1df55c9d54c42a3c02
vijaya0112 <69664353+vijaya0112@users.noreply.github.com>
2020-08-28 09:02:51 +0530

    Add files via upload

---
+++ Sum_num.java
@@ -0,0 +1,17 @@
+package pack1;
+
+public class Sum_num {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        System.out.println("sum ="+c);
+        
+	}
+
+}
