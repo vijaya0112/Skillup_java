9611e09ea9ab693e67573a02b4ba5e8104b09457
vijaya0112 <69664353+vijaya0112@users.noreply.github.com>
2020-08-28 09:04:43 +0530

    Add files via upload

---
+++ Area_Circumference.java
@@ -0,0 +1,20 @@
+package pack1;
+import java.util.*;
+public class Area_Circumference {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner obj = new Scanner(System.in);
+      
+      System.out.println("enter the value of radius");
+        int r = obj.nextInt();
+        
+        float area = 2*3.14f*r;
+      System.out.println("The area of circumference is "+area);
+	}
+
+}   
+
