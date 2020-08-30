8500ebb21d358e5eea7e62a56ac5d74a8266add6
vijaya0112 <69664353+vijaya0112@users.noreply.github.com>
2020-08-30 10:53:16 +0530

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,18 @@
+package pack1;
+public class Address {
+
+	public static void main(String[] args) {
+	 
+		String name = "Vijaya";
+		String hno = "45-40-4";
+		String city = "Rajahmundry";
+		
+		System.out.println("name = "+name);
+		System.out.println("hno =  "+hno);
+		System.out.println("city = "+city);
+
+	}
+
+	}
+
+
