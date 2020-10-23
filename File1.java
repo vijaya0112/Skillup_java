3088935dfdd0a41997132631ffac78b0289d22a1
vijaya0112 <69664353+vijaya0112@users.noreply.github.com>
2020-10-23 20:44:01 +0530

    Add files via upload

---
+++ File1.java
@@ -0,0 +1,12 @@
+package hacker_rank;
+
+public class File1 
+{
+	 public static void main(String[] args)
+     {
+         System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    
+    }
+
+}
---
+++ File2.java
@@ -0,0 +1,17 @@
+package hacker_rank;
+import java.util.*;
+public class File2 
+{
+	public static void main(String[] args) 
+	{
+    Scanner scan = new Scanner(System.in);
+	 for(int i=1 ; i<=3 ; i++)
+	  {
+       int a = scan.nextInt();
+       System.out.println(a);
+      }
+	   scan.close();
+    }
+}
+
+
