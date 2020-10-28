f6cd391e8c319a17d7045852957cb1101e6af81d
vijaya0112 <69664353+vijaya0112@users.noreply.github.com>
2020-10-28 12:19:42 +0530

    Add files via upload

---
+++ Stdin_Stdout.java
@@ -0,0 +1,26 @@
+package hacker_rank;
+import java.util.Scanner;
+
+public class Stdin_Stdout
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        double d = sc.nextDouble();
+        sc.nextLine();
+        String s = sc.nextLine();
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
+
+
+		
+
+	
+
+
